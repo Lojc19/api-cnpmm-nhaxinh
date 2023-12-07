87958486f25225f21f6f9df2caeb9bc6882f5f26
Lojc19 <89731207+Lojc19@users.noreply.github.com>
2023-12-08 00:40:11 +0700

    cors

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 const app = express()
 app.use(cors(
   {
-    origin : '*',
+    origin : 'https://cnpmm-admin.vercel.app/ localhost:3000',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true
   }
