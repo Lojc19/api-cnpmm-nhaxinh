0c8d2e8d9fdeaf53b2104be53051494b45a1c263
Bui Ba Loc <20110671@student.hcmute.edu.vn>
2024-06-20 23:18:54 +0700

    update vnpay

--- src/app/routes/order.route.js
+++ src/app/routes/order.route.js
@@ -70,12 +70,11 @@ router.post('/create_payment_url', function (req, res, next) {
     let signed = hmac.update(new Buffer(signData, 'utf-8')).digest("hex"); 
     vnp_Params['vnp_SecureHash'] = signed;
     vnpUrl += '?' + querystring.stringify(vnp_Params, { encode: false });
-    res.redirect(vnpUrl)
-    // res.json({
-    //     status: "success",
-    //     url: vnpUrl,
-    //     message: "Tao Url Thanh Cong"
-    // })
+    res.json({
+        status: "success",
+        vnpUrl: vnpUrl,
+        message: "Tao Url Thanh Cong"
+    })
 });
 
 router.get('/vnpay_return', function (req, res, next) {
@@ -103,7 +102,6 @@ router.get('/vnpay_return', function (req, res, next) {
         if(code == "00")
         {
             updateOrderSuccess(orderId);
-            res.redirect("https://www.facebook.com/BasLocc/")
             res.json({
                 status: "success",
                 message: "Thanh toán thành công"
@@ -152,6 +150,65 @@ const updateOrderFail = async(orderId) => {
       const updated = await Product.bulkWrite(update, {});
     return
 }
+
+router.get('/vnpay_ipn', function (req, res, next) {
+    let vnp_Params = req.query;
+    let secureHash = vnp_Params['vnp_SecureHash'];
+    
+    let orderId = vnp_Params['vnp_TxnRef'];
+    let rspCode = vnp_Params['vnp_ResponseCode'];
+
+    delete vnp_Params['vnp_SecureHash'];
+    delete vnp_Params['vnp_SecureHashType'];
+
+    vnp_Params = sortObject(vnp_Params);
+    let config = require('config');
+    let secretKey = config.get('vnp_HashSecret');
+    let querystring = require('qs');
+    let signData = querystring.stringify(vnp_Params, { encode: false });
+    let crypto = require("crypto");     
+    let hmac = crypto.createHmac("sha512", secretKey);
+    let signed = hmac.update(new Buffer(signData, 'utf-8')).digest("hex");     
+    
+    let paymentStatus = '0'; // Giả sử '0' là trạng thái khởi tạo giao dịch, chưa có IPN. Trạng thái này được lưu khi yêu cầu thanh toán chuyển hướng sang Cổng thanh toán VNPAY tại đầu khởi tạo đơn hàng.
+    //let paymentStatus = '1'; // Giả sử '1' là trạng thái thành công bạn cập nhật sau IPN được gọi và trả kết quả về nó
+    //let paymentStatus = '2'; // Giả sử '2' là trạng thái thất bại bạn cập nhật sau IPN được gọi và trả kết quả về nó
+    
+    let checkOrderId = true; // Mã đơn hàng "giá trị của vnp_TxnRef" VNPAY phản hồi tồn tại trong CSDL của bạn
+    let checkAmount = true; // Kiểm tra số tiền "giá trị của vnp_Amout/100" trùng khớp với số tiền của đơn hàng trong CSDL của bạn
+    if(secureHash === signed){ //kiểm tra checksum
+        if(checkOrderId){
+            if(checkAmount){
+                if(paymentStatus=="0"){ //kiểm tra tình trạng giao dịch trước khi cập nhật tình trạng thanh toán
+                    if(rspCode=="00"){
+                        //thanh cong
+                        //paymentStatus = '1'
+                        // Ở đây cập nhật trạng thái giao dịch thanh toán thành công vào CSDL của bạn
+                        res.status(200).json({RspCode: '00', Message: 'Success'})
+                    }
+                    else {
+                        //that bai
+                        //paymentStatus = '2'
+                        // Ở đây cập nhật trạng thái giao dịch thanh toán thất bại vào CSDL của bạn
+                        res.status(200).json({RspCode: '00', Message: 'Success'})
+                    }
+                }
+                else{
+                    res.status(200).json({RspCode: '02', Message: 'This order has been updated to the payment status'})
+                }
+            }
+            else{
+                res.status(200).json({RspCode: '04', Message: 'Amount invalid'})
+            }
+        }       
+        else {
+            res.status(200).json({RspCode: '01', Message: 'Order not found'})
+        }
+    }
+    else {
+        res.status(200).json({RspCode: '97', Message: 'Checksum failed'})
+    }
+});
 
 function sortObject(obj) {
 	let sorted = {};
